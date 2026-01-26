65a427f92b25ad4b569bd6267e8606c52bf83520
timtimtim.eth <timothyshen1@hotmail.com>
2026-01-26 14:53:18 +0000

    feat(teams): add component barrel export

---
+++ src/components/teams/index.ts
@@ -0,0 +1,4 @@
+export { TeamCard } from "./team-card";
+export { PendingInvites } from "./pending-invites";
+export { TeamMemberList } from "./team-member-list";
+export { InviteForm } from "./invite-form";
